00e2414941daed7ea0e966b20d9b2b294c15d175
gabriel-fv <bielfontenele@hotmail.com>
2022-06-02 21:30:40 -0300

    Criamos classe Dao, os modelos Dao e organizamos em pacotes

---
+++ TrabalhoFinal/src/trabalhofinal/modelo/Cliente.java
@@ -0,0 +1,17 @@
+package trabalhofinal.modelo;
+
+public class Cliente extends Pessoa {
+    private String ender;
+
+    public Cliente() {
+    }
+    
+    public String getEnder() {
+        return ender;
+    }
+
+    public void setEnder(String ender) {
+        this.ender = ender;
+    }   
+    
+}
